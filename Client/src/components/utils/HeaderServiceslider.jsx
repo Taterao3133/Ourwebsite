efcea8107a6bb88ea152f66008f8ca837e1fe965
Taterao Jinkalwar <122861107+Taterao3133@users.noreply.github.com>
2024-12-17 11:42:36 +0530

    bugs fixing

--- Client/src/components/utils/HeaderServiceslider.jsx
+++ Client/src/components/utils/HeaderServiceslider.jsx
@@ -40,15 +40,15 @@ function HeaderServiceslider({ isServicesHovered , setIsServicesHovered}) {
 
   return (
       <div 
-          className={`serviceHover fixed top-[80px] left-0 w-full h-auto transition-all duration-500 ease-in-out ${isSticky ? 'top-[63px]': 'top-14'} ${isServicesHovered ? 'opacity-100 z-50 top-10 ' : '  left-auto opacity-0 pointer-events-none -top-5'} z-40`}
+          className={`serviceHover fixed top-[80px] left-0 w-full h-auto transition-all duration-500 ease-in-out ${isSticky ? 'top-[63px]': 'top-14'} ${isServicesHovered ? 'opacity-100 z-50 top-10 ' : '  left-auto opacity-0 pointer-events-none -top-4'} z-40`}
           onMouseEnter={() => setIsServicesHovered(true)}  // 
             onMouseLeave={() => setIsServicesHovered(false)} // 
           >
-          <div className="bx w-[85%] h-auto pb-5 border-2 pt-5 px-7  rounded-bl-md rounded-br-md mx-auto bg-[#FFFFFF]">
+          <div className="bx w-[100%] h-auto pb-5 border-2 pt-5 px-7 xl:px-24  rounded-bl-md rounded-br-md mx-auto bg-[#FFFFFF]">
              <div className="col">
-              <div className="c flex  h-auto gap-x-12">
-                <div className="space-y-8">
-                  <div className="header-s"><h2 className=" font-roboto font-medium text-xl text-center text-[#0C3461]">MOBILE APP DEVELOPMENT</h2>
+              <div className="c flex  h-auto lg:gap-x-8 xl:gap-x-12">
+                <div className="lg:space-y-4 xl:space-y-8 max-xl:text-[15px] xl:text-[16px] font-roboto-serif font-medium">
+                  <div className="header-s"><h2 className=" font-roboto font-medium xl:text-xl max-xl:text-[17px]   text-center text-[#0C3461]">MOBILE APP DEVELOPMENT</h2>
                   <hr className="text-[#D9D9D9] w-full border-1"/>
 
                   </div>
@@ -56,128 +56,128 @@ function HeaderServiceslider({ isServicesHovered , setIsServicesHovered}) {
                     <div className="icons">
                       <img src={CAD} alt="" />
                     </div>
-                    <p className="mt-2 ml-2 text-[#302F68] cursor-pointer font-roboto-serif font-medium text-[16px]">Custom App Development</p>
+                    <p className="mt-2 ml-2 text-[#302F68] cursor-pointer">Custom App Development</p>
                   </div>
                   <div className="flex">
                     <div className="icons">
                       <img src={CPAD} alt="" />
                     </div>
-                    <p className="ml-2  text-[#302F68] cursor-pointer font-roboto-serif font-medium text-[16px]">Cross-Platform App <br /> Development </p>
+                    <p className="ml-2  text-[#302F68] cursor-pointer">Cross-Platform App <br /> Development </p>
                   </div>
                   <div className="flex">
                     <div className="icons">
                       <img src={AAD} alt="" />
                     </div>
-                    <p className="mt-2 ml-2 text-[#302F68] cursor-pointer font-roboto-serif font-medium text-[16px]">Android App Development</p>
+                    <p className="mt-2 ml-2 text-[#302F68] cursor-pointer">Android App Development</p>
                   </div>
                   <div className="flex">
                     <div className="icons">
                       <img src={MAD} alt="" />
                     </div>
-                    <p className="mt-2 ml-2 text-[#302F68] cursor-pointer font-roboto-serif font-medium text-[16px]">Mobile App Designing</p>
+                    <p className="mt-2 ml-2 text-[#302F68] cursor-pointer">Mobile App Designing</p>
                   </div>
                 </div>
 
-                <div className=" space-y-8 h-auto">
-                  <div className="header-s"><h2 className="font-roboto font-medium text-xl text-center text-[#0C3461]">WEBSITE DEVELOPMENT</h2>
+                <div className=" lg:space-y-4 xl:space-y-8 h-auto max-xl:text-[15px] xl:text-[16px] font-roboto-serif font-medium">
+                  <div className="header-s"><h2 className="font-roboto font-medium xl:text-xl max-xl:text-[17px] text-center text-[#0C3461]">WEBSITE DEVELOPMENT</h2>
                   <hr className="text-[#D9D9D9] w-full border-1"/>
                   </div>
                   <div className="flex">
                     <div className="icons">
                       <img src={CWD} alt="" />
                     </div>
-                    <p className="ml-2 text-[#302F68] cursor-pointer font-roboto-serif font-medium text-[16px]">Custom Website <br /> Development</p>
+                    <p className="ml-2 text-[#302F68] cursor-pointer ">Custom Website <br /> Development</p>
                   </div>
                   <div className="flex">
                     <div className="icons">
                       <img src={EWD} alt="" />
                     </div>
-                    <p className="ml-2 text-[#302F68] cursor-pointer font-roboto-serif font-medium text-[16px]">E-commerce Website <br /> Development</p>
+                    <p className="ml-2 text-[#302F68] cursor-pointer ">E-commerce Website <br /> Development</p>
                   </div>
                   <div className="flex">
                     <div className="icons">
                       <img src={RWD} alt="" />
                     </div>
-                    <p className="mt-2 ml-2 text-[#302F68] cursor-pointer font-roboto-serif font-medium text-[16px]">Responsive Web Design  </p>
+                    <p className="mt-2 ml-2 text-[#302F68] cursor-pointer ">Responsive Web Design  </p>
                   </div>
                   <div className="flex">
                     <div className="icons">
                       <img src={WT} alt="" />
                     </div>
-                    <p className="mt-2 ml-2 text-[#302F68] cursor-pointer font-roboto-serif font-medium text-[16px]">Website Testing</p>
+                    <p className="mt-2 ml-2 text-[#302F68] cursor-pointer ">Website Testing</p>
                   </div>
                   <div className="flex">
                     <div className="icons">
                       <img src={WM} alt="" />
                     </div>
-                    <p className="mt-2 ml-2 text-[#302F68] cursor-pointer font-roboto-serif font-medium text-[16px]">Website Maintenance</p>
+                    <p className="mt-2 ml-2 text-[#302F68] cursor-pointer ">Website Maintenance</p>
                   </div>
                   <div className="flex">
                     <div className="icons">
                       <img src={LP} alt="" />
                     </div>
-                    <p className="mt-2 ml-2 text-[#302F68] cursor-pointer font-roboto-serif font-medium text-[16px]">Landing Pages</p>
+                    <p className="mt-2 ml-2 text-[#302F68] cursor-pointer ">Landing Pages</p>
                   </div>
                 </div>
 
-                <div className=" space-y-8">
-                  <div className="header-s"><h2 className=" font-roboto font-medium text-xl text-center text-[#0C3461]">UI/UX DESIGNE SERVICES</h2>
+                <div className="lg:space-y-4 xl:space-y-8 max-xl:text-[15px] xl:text-[16px] font-roboto-serif font-medium">
+                  <div className="header-s"><h2 className=" font-roboto font-medium xl:text-xl max-xl:text-[17px] text-center text-[#0C3461]">UI/UX DESIGNE SERVICES</h2>
                   <hr className="text-[#D9D9D9] w-full border-1"/>
                   </div>
                   <div className="flex">
                     <div className="icons">
                       <img src={R} alt="" />
                     </div>
-                    <p className="mt-2 ml-2 text-[#302F68] cursor-pointer font-roboto-serif font-medium text-[16px]">Research</p>
+                    <p className="mt-2 ml-2 text-[#302F68]">Research</p>
                   </div>
                   <div className="flex">
                     <div className="icons">
                       <img src={W} alt="" />
                     </div>
-                    <p className="mt-2 ml-2 text-[#302F68] cursor-pointer font-roboto-serif font-medium text-[16px]">Wireframing</p>
+                    <p className="mt-2 ml-2 text-[#302F68]">Wireframing</p>
                   </div>
                   <div className="flex">
                     <div className="icons">
                       <img src={P} alt="" />
                     </div>
-                    <p className="mt-2 ml-2 text-[#302F68] cursor-pointer font-roboto-serif font-medium text-[16px]">Prototyping</p>
+                    <p className="mt-2 ml-2 text-[#302F68]">Prototyping</p>
                   </div>
                   <div className="flex">
                     <div className="icons">
                       <img src={VD} alt="" />
                     </div>
-                    <p className="mt-2 ml-2 text-[#302F68] cursor-pointer font-roboto-serif font-medium text-[16px]">Visual Design</p>
+                    <p className="mt-2 ml-2 text-[#302F68]">Visual Design</p>
                   </div>
                   <div className="flex">
                     <div className="icons">
                       <img src={ID1} alt="" />
                     </div>
-                    <p className="mt-2 ml-2 text-[#302F68] cursor-pointer font-roboto-serif font-medium text-[16px]">Interaction Design</p>
+                    <p className="mt-2 ml-2 text-[#302F68]">Interaction Design</p>
                   </div>
                   <div className="flex">
                     <div className="icons">
                       <img src={IA} alt="" />
                     </div>
-                    <p className=" ml-2 text-[#302F68] cursor-pointer font-roboto-serif font-medium text-[16px]">Information Architecture</p>
+                    <p className=" ml-2 text-[#302F68]">Information Architecture</p>
                   </div>
                 </div>
                 {/* knk */}
 
-                <div className=" space-y-8">
-                  <div className="header-s"><h2 className=" font-roboto font-medium text-xl text-center text-[#0C3461]">DIGITAL MARKENTING SERVICES</h2>
+                <div className="lg:space-y-4 xl:space-y-8 max-xl:text-[15px] xl:text-[16px] font-roboto-serif font-medium ">
+                  <div className="header-s"><h2 className=" font-roboto font-medium xl:text-xl max-xl:text-[17px] text-center text-[#0C3461]">DIGITAL MARKENTING SERVICES</h2>
                   <hr className="text-[#D9D9D9] w-full border-1"/>
                   </div>
                   <div className="flex">
                     <div className="icons">
                       <img src={SM} alt="" />
                     </div>
-                    <p className="mt-2 ml-2 text-[#302F68] cursor-pointer font-roboto-serif font-medium text-[16px]">Social Media Marketing</p>
+                    <p className="mt-2 ml-2 text-[#302F68] cursor-pointer">Social Media Marketing</p>
                   </div>
                   <div className="flex">
                     <div className="icons">
                       <img src={G} alt="" />
                     </div>
-                    <p className="mt-2 ml-2 text-[#302F68] cursor-pointer font-roboto-serif font-medium text-[16px]">Graphic Design</p>
+                    <p className="mt-2 ml-2 text-[#302F68] cursor-pointer ">Graphic Design</p>
                   </div>
                 </div> 
               </div>
